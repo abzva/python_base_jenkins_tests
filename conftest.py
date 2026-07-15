43f7c8a19dce4af8519853b606197cd550722bca
Anastasuy092 <bazovaanastasia@yandex.ru>
2026-07-15 11:59:03 +0300

    add attach

--- conftest.py
+++ conftest.py
@@ -6,6 +6,16 @@ from selenium.webdriver.chrome.options import Options
 def driver():
     options = Options()
 
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": '128.0',
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+
     driver = webdriver.Remote(
         command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
         options=options
@@ -14,3 +24,10 @@ def driver():
     # driver = webdriver.Chrome(options=options)
 
     yield driver
+
+    attach.add_screenshot(driver)
+    attach.add_page_source(driver)
+    attach.add_console_logs(driver)
+    attach.add_video(driver)
+
+    driver.quit()
